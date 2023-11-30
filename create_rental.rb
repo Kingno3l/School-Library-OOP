3cf05ee78255f74d41bd1b25031cabe32bf1095e
BHObserver <BHObserver0@gmail.com>
2023-11-30 10:06:57 +0600

    Update create_rental.rb

--- create_rental.rb
+++ create_rental.rb
@@ -1,13 +1,15 @@
-def list_books_for_selection(books)
-  puts 'Select a book from the following list by number:'
-  books.each_with_index do |book, index|
-    puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+module RentalHelper
+  def self.list_books_for_selection(books)
+    puts 'Select a book from the following list by number:'
+    books.each_with_index do |book, index|
+      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+    end
   end
-end
 
-def list_people_for_selection(peoples)
-  puts 'Select a person from the following list by number (not id):'
-  peoples.each_with_index do |person, index|
-    puts "#{index}) [#{person.class}] Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
+  def self.list_people_for_selection(people)
+    puts 'Select a person from the following list by number (not id):'
+    people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
+    end
   end
 end
